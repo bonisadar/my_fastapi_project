2b178bbc2aa21035b3afaf5755d3cbf862af35b4
bonisadar <bonisadar@gmail.com>
2025-05-04 16:44:17 +0600

    deleted app

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pickle
 import os
 from fastapi import FastAPI, HTTPException
 
-# from app.utils.preprocess import preprocess_url_single
+from app.utils.preprocess import preprocess_url_single
 from app.models import predictor
 from app.models.predictor import load_model, predict
 
