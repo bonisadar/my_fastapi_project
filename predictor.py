072ac9c8d549c75fea1248c68a6ddb7d148bde09
bonisadar <bonisadar@gmail.com>
2025-05-04 22:54:33 +0600

    model moved

--- predictor.py
+++ predictor.py
@@ -5,7 +5,7 @@ import re
 import numpy as np
 
 def load_model():
-    model_path = os.getenv("MODEL_PATH", "app/models/model.pkl")
+    model_path = os.getenv("MODEL_PATH", "model.pkl")
     try:
         with open(model_path, "rb") as f:
             model = pickle.load(f)
